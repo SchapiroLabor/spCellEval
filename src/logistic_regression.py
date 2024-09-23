3bcb4d458684a8146470c2c44598515707c1a40b
Lukas Hatscher <lhatscher@protonmail.com>
2024-09-23 22:33:08 +0200

    work on logistic regression file

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -2,14 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (
-    confusion_matrix,
-    accuracy_score,
-    classification_report,
-    f1_score,
-    precision_score,
-    recall_score
-)
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, f1_score, precision_score, recall_score
 from sklearn.preprocessing import StandardScaler
 
 class MultinomialLogisticRegression:
@@ -51,16 +44,16 @@ class MultinomialLogisticRegression:
             accuracy = accuracy_score(Y_test, predictions)
             f1 = f1_score(Y_test, predictions, average='macro')
             weighted_f1 = f1_score(Y_test, predictions, average='weighted')
-            confusion_matrix = confusion_matrix(Y_test, predictions)
+            cm = confusion_matrix(Y_test, predictions)
             precision = precision_score(Y_test, predictions, average='weighted')
             recall = recall_score(Y_test, predictions, average='weighted')
-            classification_report = classification_report(Y_test, predictions)
+            cr = classification_report(Y_test, predictions)
 
             self.fold_accuracies.append(accuracy)
             self.fold_f1_scores.append(f1)
             self.fold_weighted_f1_scores.append(weighted_f1)
-            self.confusion_matrices.append(confusion_matrix)
-            self.classification_reports.append(classification_report)
+            self.confusion_matrices.append(cm)
+            self.classification_reports.append(cr)
 
             print(f"Fold {i+1} results:")
             print(f"Precision: {precision}")
@@ -117,16 +110,16 @@ class MultinomialLogisticRegression:
             accuracy = accuracy_score(Y_test, predictions)
             f1 = f1_score(Y_test, predictions, average='macro')
             weighted_f1 = f1_score(Y_test, predictions, average='weighted')
-            confusion_matrix = confusion_matrix(Y_test, predictions)
+            cm = confusion_matrix(Y_test, predictions)
             precision = precision_score(Y_test, predictions, average='weighted')
             recall = recall_score(Y_test, predictions, average='weighted')
-            classification_report = classification_report(Y_test, predictions)
+            cr = classification_report(Y_test, predictions)
 
             self.fold_accuracies.append(accuracy)
             self.fold_f1_scores.append(f1)
             self.fold_weighted_f1_scores.append(weighted_f1)
-            self.confusion_matrices.append(confusion_matrix)
-            self.classification_reports.append(classification_report)
+            self.confusion_matrices.append(cm)
+            self.classification_reports.append(cr)
 
             print(f"Fold {i+1} results:")
             print(f"Precision: {precision}")
@@ -149,45 +142,3 @@ class MultinomialLogisticRegression:
         print(f"Average Recall across all folds: {self.average_recall}")
 
         
-        for i, fold in enumerate(self.data_handler.fold_data):
-            X_train = self.scaler.fit_transform(fold['X_train'])
-            X_test = self.scaler.transform(fold['X_test'])
-            Y_train = fold['Y_train']
-            Y_test = fold['Y_test']
-            
-            self.model.fit(X_train, Y_train)
-            predictions = self.model.predict(X_test)
-
-            accuracy = accuracy_score(Y_test, predictions)
-            f1 = f1_score(Y_test, predictions, average='macro')
-            weighted_f1 = f1_score(Y_test, predictions, average='weighted')
-            confusion_matrix = confusion_matrix(Y_test, predictions)
-            precision = precision_score(Y_test, predictions, average='weighted')
-            recall = recall_score(Y_test, predictions, average='weighted')
-            classification_report = classification_report(Y_test, predictions)
-
-            self.fold_accuracies.append(accuracy)
-            self.fold_f1_scores.append(f1)
-            self.fold_weighted_f1_scores.append(weighted_f1)
-            self.confusion_matrices.append(confusion_matrix)
-            self.classification_reports.append(classification_report)
-
-            print(f"Fold {i+1} results:")
-            print(f"Precision: {precision}")
-            print(f"Recall: {recall}")
-            print("Accuracy:", accuracy)
-            print("F1 Score:", f1)
-            print("Weighted F1 Score:", weighted_f1)
-            print("="*30)
-        
-        self.average_accuracy = np.mean(self.fold_accuracies)
-        self.average_f1_score = np.mean(self.fold_f1_scores)
-        self.average_weighted_f1_score = np.mean(self.fold_weighted_f1_scores)
-        self.average_precision = np.mean(self.fold_precisions)
-        self.average_recall = np.mean(self.fold_recalls)
-
-        print(f"Average Accuracy across all folds: {self.accuracy}")
-        print(f"Average F1 Score across all folds: {self.average_f1_score}")
-        print(f"Average Weighted F1 Score across all folds: {self.average_weighted_f1_score}")
-        print(f"Average Precision across all folds: {self.average_precision}")
-        print(f"Average Recall across all folds: {self.average_recall}")
