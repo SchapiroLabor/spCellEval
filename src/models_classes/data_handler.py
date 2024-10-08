2849425b503593ee89043ffd1400e13f3d4fa5d7
Lukas Hatscher <lhatscher@protonmail.com>
2024-10-08 11:05:10 +0200

    added valdiation split

--- src/models_classes/data_handler.py
+++ src/models_classes/data_handler.py
@@ -2,7 +2,7 @@ import os
 import json
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold
+from sklearn.model_selection import StratifiedGroupKFold, StratifiedKFold, train_test_split
 from sklearn.preprocessing import LabelEncoder
 from typing import List
 
@@ -21,10 +21,10 @@ class DataSetHandler:
         print("DataSetHandler initialized successfully")
     
     
-    def preprocess(self, dropna: bool, impute_value: float = None, phenotype_column: str = 'cell_type', group_identifier_column: str = None, drop_columns: List[str] = None) -> None:
+    def preprocess(self, dropna: bool, impute_value: float = None, phenotype_column: str = 'cell_type', group_identifier_column: str = None, drop_columns: List[str] = None, drop_non_numerical: bool = False) -> None:
         """
         This function processes the laoded dataframe. If NA values are present, they can be dropped or imputed with a specified value. A group identifier can be
-        specified so the kfolds are created with the group identifier in mind. The phenotype column is encoded and the data is split into X and Y. 
+        specified so which will be kept even if non-numerical columns are dropped. The phenotype column is encoded and the data is split into X and Y. 
         """
 
         if not isinstance(dropna, bool):
@@ -45,13 +45,17 @@ class DataSetHandler:
             'label': range(len(label_encoder.classes_)),
             'phenotype': label_encoder.classes_
             })
-        if group_identifier_column is not None:
-            self.group_identifier = self.data[group_identifier_column]
-            self.X = self.data.drop(columns=[phenotype_column, group_identifier_column])
-            self.X = self.X.select_dtypes(include=[np.number])
+        
+        # If group identifier is selected but is a non-numerical and user selects to drop non-numericals, it is stored in a separate variable and re-inserted to preserve it
+        if drop_non_numerical:
+            if group_identifier_column is not None and self.data[group_identifier_column].dtype is not np.number:
+                self.group_identifier = self.data[group_identifier_column]
+                self.X = self.data.select_dtypes(include=[np.number])
+                self.X.insert(len(self.X.columns), group_identifier_column, self.group_identifier)
+            else:
+                self.X  = self.data.select_dtypes(include=[np.number])
         else:
             self.X = self.data.drop(columns=[phenotype_column])
-            self.X = self.X.select_dtypes(include=[np.number])
 
         print("Data successfully preprocessed")
     
@@ -67,20 +71,14 @@ class DataSetHandler:
 
     def createKfold(self, k: int) -> None:
         """
-        Creates folds, depending if the user has specified a group identifier or not. If a group identifier is present, StratifiedGroupKFold is used, otherwise StratifiedKFold is used.
+        Creates StratifiedKfolds.
         Folds will be carried by the fold_data attribute.
         """
         if not isinstance(k, int):
             raise TypeError("k must be an integer")
         
-        if self.group_identifier is not None:
-            print("Group identifier column specified, StratifiedGroupKFold will be used")
-            self.kfolds = StratifiedGroupKFold(n_splits=k, shuffle=True, random_state=self.random_state)
-            fold_generator = self.kfolds.split(self.X, self.Y, self.group_identifier)
-        else:
-            print("No group identifier column specified, StratifiedKFold will be used")
-            self.kfolds = StratifiedKFold(n_splits=k, random_state=self.random_state, shuffle=True)
-            fold_generator = self.kfolds.split(self.X, self.Y)
+        self.kfolds = StratifiedKFold(n_splits=k, random_state=self.random_state, shuffle=True)
+        fold_generator = self.kfolds.split(self.X, self.Y)
 
         self.fold_indices = list(fold_generator)
         
@@ -113,7 +111,7 @@ class DataSetHandler:
 
         fold_data = {
             'random_state': self.random_state,
-            'folds': [{'train': train.tolist(), 'test': test.tolist()} for train, test in self.fold_indices]
+            'folds': [{'fold': i+1, 'train': train.tolist(), 'test': test.tolist()} for i, (train, test) in enumerate(self.fold_indices)]
         }
 
 
@@ -129,6 +127,38 @@ class DataSetHandler:
             
             train_data.to_csv(os.path.join(kfolds_dir, f'fold_{i+1}_train.csv'), index=False)
             test_data.to_csv(os.path.join(kfolds_dir, f'fold_{i+1}_test.csv'), index=False)
-
+        
         print(f"Folds saved in: {kfolds_dir}")
 
+    def create_validation_set_from_fold(self, save_path: str, percentage_validation: float = 0.15):
+        """
+        Creates validation sets for each pre-existing training fold. The percentage of data to be used as validation can be specified. The folds have to be created before by calling createKfold
+        and saved by calling save_folds. The pre-existing training folds will be overwritten.
+        """
+        # Load training folds and train test split
+        if not os.path.exists(save_path):
+            raise ValueError(f"The specified save_path {save_path} does not exist.")
+
+        with open(os.path.join(save_path, 'fold_indices.json'), 'r') as f:
+            fold_data = json.load(f)
+
+        for i, fold in enumerate(fold_data['folds']):
+            train_data_original = pd.read_csv(os.path.join(save_path, f'fold_{i+1}_train.csv'))
+            train_data, validation_data = train_test_split(train_data_original, test_size=percentage_validation, random_state=self.random_state, stratify=train_data_original['encoded_phenotype'])
+            print(f"Overwriting 'fold_{i+1}_train.csv'... and saving validation")
+            train_file_path = os.path.join(save_path, f'fold_{i+1}_train.csv')
+            validation_file_path = train_file_path.replace('train', 'validation')
+            validation_data.to_csv(validation_file_path, index=False)
+            train_data.to_csv(train_file_path, index=False)
+
+            train_indices = train_data.index.tolist()
+            validation_indices = validation_data.index.tolist()
+
+            fold['train'] = train_indices
+            fold['validation'] = validation_indices
+
+                # Save the updated fold indices back to the JSON file
+        with open(os.path.join(save_path, 'fold_indices.json'), 'w') as f:
+            json.dump(fold_data, f)
+
+        print("Validation indices updated in fold_indices.json")
