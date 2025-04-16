4ff13aee479cb6b826ab81912abeadd8bf330cf9
Lukas Hatscher <lhatscher@protonmail.com>
2025-04-16 14:01:10 +0200

    batchscaling scripts

---
+++ src/models_classes/default_classic_ml_models_kfolds_batchscale.py
@@ -0,0 +1,326 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, f1_score, precision_score, recall_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier as rfc
+from xgboost import XGBClassifier
+from sklearn.utils.class_weight import compute_sample_weight
+import json
+import csv
+import pickle
+
+class ClassicMLDefault:
+    def __init__(self, random_state: int, model: str, n_jobs: str, **kwargs) -> None:
+        if model == 'logistic_regression':
+            self.model_name = 'logistic_regression'
+            self.model = LogisticRegression(n_jobs=n_jobs, random_state=random_state, **kwargs)
+        elif model == 'random_forest':
+            self.model_name = 'random_forest'
+            self.model = rfc(n_jobs=n_jobs, random_state=random_state, criterion='log_loss', **kwargs)
+        elif model == 'xgboost':
+            self.class_weight = kwargs.pop('class_weight', None)
+            self.model_name = 'xgboost'
+            self.model = XGBClassifier(objective = 'multi:softmax', eval_metric = 'mlogloss', booster = 'gbtree', n_jobs=n_jobs, random_state=random_state, **kwargs)
+        else:
+            raise ValueError("Invalid model. Please choose either 'logistic_regression', 'xgboost' or 'random_forest' as model parameter.")
+
+        self.kwargs = kwargs
+        self.random_state = random_state
+        self.data_handler = None
+        self.scaler = StandardScaler()
+        self.fold_accuracies = []
+        self.fold_f1_scores = []
+        self.fold_weighted_f1_scores = []
+        self.fold_precisions = []
+        self.fold_recalls = []
+        self.confusion_matrices = []
+        self.classification_reports = []
+        self.average_accuracy = None
+        self.average_f1_score = None
+        self.average_weighted_f1_score = None
+        self.average_precision = None
+        self.average_recall = None
+        self.best_models = []
+        self.predictions = {}
+
+        print('Class initialized successfully with the following model parameters:')
+        print(f'Random State: {self.random_state}')
+        print(f'Model: {self.model}')
+
+    def _zscore_scaling(self, df, features, scale_batch_col=None):
+            df_scaled = df.copy()
+            df_scaled[features] = df_scaled[features].astype(np.float64)
+            if scale_batch_col is not None and scale_batch_col in df.columns:
+                for batch in df[scale_batch_col].unique():
+                    batch_mask = df[scale_batch_col] == batch
+                    df_scaled.loc[batch_mask, features] = self.scaler.fit_transform(df.loc[batch_mask, features])
+            else:
+                df_scaled[features] = self.scaler.fit_transform(df[features])
+            return df_scaled
+
+
+    def train_tune_evaluate(self, path: str, label_path:str, verbose:int = 2, scaling:bool = True, scale_batch_col:str = None, dumb_columns: str | list = None, dumb_nonnumericals: bool = True) -> None:
+        """
+        This function implements manual kfold cross-validation using a custom parameter grid and evaluates the models based on predefined and saved kfolds. 
+        Input is the path pointing to the folder containing the train, validation and test csv kfold files.
+        """ 
+
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The path {path} does not exist.")
+        
+        fold_dict = {
+            'train': [],
+            'validation': [],
+            'test': []
+        }
+        for file in os.listdir(path):
+            if file.endswith('.csv'):
+                if 'train' in file:
+                    fold_dict['train'].append(file)
+                elif 'validation' in file:
+                    fold_dict['validation'].append(file) 
+                elif 'test' in file:
+                    fold_dict['test'].append(file)
+                else:
+                    print(f"skipping {file}")
+
+        fold_dict['train'].sort()
+        fold_dict['validation'].sort()
+        fold_dict['test'].sort()
+
+        if len(fold_dict['train']) != len(fold_dict['test']) or len(fold_dict['train']) != len(fold_dict['validation']):
+            raise ValueError("The number of train, validation and test files do not match.")
+        
+        labels = pd.read_csv(label_path)
+        label_dict = dict(zip(labels['label'], labels['phenotype']))
+        all_labels = list(label_dict.keys())
+        
+
+        print('Starting the integration of the predefined Kfolds...')
+        for i, (train_file, validation_file, test_file) in enumerate(zip(fold_dict['train'], fold_dict['validation'], fold_dict['test'])):
+            if dumb_columns is not None:
+                train_data = pd.read_csv(os.path.join(path, train_file))
+                validation_data = pd.read_csv(os.path.join(path, validation_file))
+                test_data = pd.read_csv(os.path.join(path, test_file))
+                #check if columns in dumb_columns are in the dataframes
+                dumb_columns = [col for col in dumb_columns if col in train_data.columns]
+                train_data = train_data.drop(columns=dumb_columns)
+                validation_data = validation_data.drop(columns=dumb_columns)
+                test_data = test_data.drop(columns=dumb_columns)
+            else:
+                train_data = pd.read_csv(os.path.join(path, train_file))
+                validation_data = pd.read_csv(os.path.join(path, validation_file))
+                test_data = pd.read_csv(os.path.join(path, test_file))
+
+
+            if dumb_nonnumericals:
+                def keep_batch_and_numerics(df):
+                    if scale_batch_col and scale_batch_col in df.columns:
+                        batch_col = df[[scale_batch_col]]
+                        numeric_cols = df.select_dtypes(include=[np.number]).columns
+                        return df[numeric_cols].join(batch_col)
+                    return df.select_dtypes(include=[np.number])
+
+                train_data = keep_batch_and_numerics(train_data)
+                validation_data = keep_batch_and_numerics(validation_data)
+                test_data = keep_batch_and_numerics(test_data)
+            if scale_batch_col is not None and scale_batch_col in train_data.columns:
+                non_numeric_df = train_data.drop(columns=scale_batch_col).select_dtypes(exclude=[np.number])
+            else:
+                non_numeric_df = train_data.select_dtypes(exclude=[np.number])
+            if train_data.isnull().values.any() or validation_data.isnull().values.any() or test_data.isnull().values.any():
+                raise ValueError("NaN values found in the fold data. Please handle missing values before training.")
+            elif not non_numeric_df.empty:
+                raise ValueError(f"Non-numeric values found in the fold data. Please encode the non-numeric values before training. Found columns: {non_numeric_df.columns}")
+            else:
+                print(f"Taking data from {train_file} and {validation_file} for fold {i+1}. No NANs found")
+            
+            # Scaling 
+            if scaling:
+                if scale_batch_col is not None and scale_batch_col in train_data.columns:
+                    print(f"Scaling data with batch column {scale_batch_col}")
+                    features = [col for col in train_data.columns if col not in ['encoded_phenotype', scale_batch_col]]
+                    train_data = self._zscore_scaling(train_data, features, scale_batch_col)
+                    validation_data = self._zscore_scaling(validation_data, features, scale_batch_col)
+                    test_data = self._zscore_scaling(test_data, features, scale_batch_col)
+                    train_data = train_data.drop(columns=scale_batch_col)
+                    validation_data = validation_data.drop(columns=scale_batch_col)
+                    test_data = test_data.drop(columns=scale_batch_col)
+                
+                else:
+                    print(f"Scaling data without batch column")
+                    features = [col for col in train_data.columns if col != 'encoded_phenotype']
+                    train_data = self._zscore_scaling(train_data, features)
+                    validation_data =self. _zscore_scaling(validation_data, features)
+                    test_data = self._zscore_scaling(test_data, features)
+
+            X_train = train_data.drop(columns='encoded_phenotype')
+            y_train = train_data['encoded_phenotype'].values
+            X_val = validation_data.drop(columns='encoded_phenotype')
+            y_val = validation_data['encoded_phenotype'].values
+            X_test = test_data.drop(columns='encoded_phenotype')
+            y_test = test_data['encoded_phenotype'].values
+
+
+            print(f"Fold {i+1} integrated successfully")
+
+            if self.model_name == 'xgboost':
+                if self.class_weight is not None:
+                    sample_weights = compute_sample_weight(class_weight=self.class_weight,y=y_train)
+                    self.model.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=verbose, sample_weight=sample_weights)
+                else:
+                    self.model.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=verbose)
+            else:
+                self.model.fit(X_train, y_train)
+
+            y_pred_test = self.model.predict(X_test)
+
+            accuracy = accuracy_score(y_test, y_pred_test)
+            f1 = f1_score(y_test, y_pred_test, average='macro')
+            weighted_f1 = f1_score(y_test, y_pred_test, average='weighted')
+            precision = precision_score(y_test, y_pred_test, average='macro')
+            recall = recall_score(y_test, y_pred_test, average='macro')
+            cm = confusion_matrix(y_test, y_pred_test, labels=all_labels)
+            cr = classification_report(y_test, y_pred_test, output_dict=False)
+
+            self.predictions[f'fold_{i+1}'] = y_pred_test
+            self.fold_accuracies.append(accuracy)
+            self.fold_f1_scores.append(f1)
+            self.fold_weighted_f1_scores.append(weighted_f1)
+            self.fold_precisions.append(precision)
+            self.fold_recalls.append(recall)
+            self.confusion_matrices.append(cm)
+            self.classification_reports.append(cr)
+            self.best_models.append(self.model)
+            print(f"Test Fold {i+1} completed")
+            print(f"classification report: {cr}")
+
+            del X_train, X_val, y_train, y_val, X_test, y_test
+
+           
+
+
+        print('Calculating average results...')
+        self.average_accuracy = np.mean(self.fold_accuracies)
+        self.average_f1_score = np.mean(self.fold_f1_scores)
+        self.average_weighted_f1_score = np.mean(self.fold_weighted_f1_scores)
+        self.average_precision = np.mean(self.fold_precisions)
+        self.average_recall = np.mean(self.fold_recalls)
+        print(f'Average Accuracy: {self.average_accuracy}')
+        print(f'Average F1 Score: {self.average_f1_score}')
+        print(f'Average Weighted F1 Score: {self.average_weighted_f1_score}')
+
+
+
+    def save_results(self, save_path: str, label_path: str, data_path:str, save_model: bool = True) -> None:
+            """
+            This function saves the results of the model and translates the labels to their respective phenotypes.
+            """
+            if label_path is None:
+                raise ValueError("Please provide the path to the label file.")
+            
+            if not os.path.exists(save_path):
+                print(f"The path {save_path} does not exist. Creating directory 'results' in the current working directory.")
+                save_path = os.path.join(os.getcwd(), 'results')
+                os.makedirs(save_path, exist_ok=True)
+            else:
+                print(f"The path {save_path} exists. Saving results in the specified directory.")
+
+            # This part saves the average results in a .json file
+            avg_results = {
+                'average_accuracy': self.average_accuracy,
+                'average_f1_score': self.average_f1_score,
+                'average_weighted_f1_score': self.average_weighted_f1_score,
+                'average_precision': self.average_precision,
+                'average_recall': self.average_recall,
+            }
+
+            print("Saving average results...")
+            if self.model_name == 'random_forest':
+                json_name = 'average_rfc_results.json'
+            elif self.model_name == 'logistic_regression':
+                json_name = 'average_logreg_results.json'
+            elif self.model_name == 'xgboost':
+                json_name = 'average_xgboost_results.json'
+            with open(os.path.join(save_path, json_name), 'w') as f:
+                json.dump(avg_results, f, indent=4)
+
+
+            labels = pd.read_csv(label_path)
+            label_dict = dict(zip(labels['label'], labels['phenotype']))
+
+            # This part saves the confusion matrices
+            def __create_labeled_cm(cm, label_dict):
+                df = pd.DataFrame(cm, columns=label_dict.values(), index=label_dict.values())
+                df.index.name = 'Actual'
+                df.columns.name = 'Predicted'
+                return df
+
+            print("Saving confusion matrices...")
+
+            for fold, cm in enumerate(self.confusion_matrices, 1):
+                labeled_cm = __create_labeled_cm(cm, label_dict)
+                csv_filename = f'confusion_matrix_fold_{fold}.csv'
+                labeled_cm.to_csv(os.path.join(save_path, csv_filename))
+            
+            # This part saves the classification reports
+            def __translate_and_save_report(report_str, label_dict, fold):
+                lines = report_str.strip().split('\n')
+
+                header = lines[0].split()
+                header.insert(0, 'label')
+
+                data_rows = []
+                for line in lines[2:]:  # Skip the header and the empty line
+                    if line.strip() and not line.startswith('accuracy') and not line.startswith('macro avg') and not line.startswith('weighted avg'):
+                        parts = line.split()
+                        if len(parts) == 5:  # Ensure it's a data row
+                            label_num = int(parts[0])
+                            label_name = label_dict.get(label_num, str(label_num))
+                            data_rows.append([label_name] + parts[1:])
+
+                summary_rows = []
+                for line in lines[-3:]:
+                    parts = line.split()
+                    if parts[0] == 'accuracy':
+                        summary_rows.append(['accuracy', '', '', parts[1], parts[2]])  # Add an empty field for alignment
+                    elif parts[0] == 'macro' or parts[0] == 'weighted':
+                        summary_rows.append([f"{parts[0]}avg", parts[2], parts[3], parts[4], parts[5]])
+
+                all_rows = data_rows + summary_rows
+
+                output_file = os.path.join(save_path, f'classification_report_fold_{fold}.csv')
+                with open(output_file, 'w', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(header)
+                    writer.writerows(all_rows)
+
+            print("Saving classification reports...")
+            for i, cr in enumerate(self.classification_reports, 1):
+                __translate_and_save_report(cr, label_dict, i)
+
+            print('Saving predictions on test csv...')
+            for i, (key, value) in enumerate(self.predictions.items(), 1):
+                test_data = pd.read_csv(os.path.join(data_path, f'fold_{i}_test.csv'))
+                test_data['predicted_phenotype'] = value
+                test_data['predicted_phenotype'] = test_data['predicted_phenotype'].map(label_dict)
+                test_data['true_phenotype'] = test_data['encoded_phenotype'].map(label_dict)
+                test_data.drop(columns='encoded_phenotype', inplace=True)
+                test_data.to_csv(os.path.join(save_path, f'predictions_fold_{i}.csv'), index=False)
+
+            # This part saves the model
+            if save_model:
+                models_path = os.path.join(save_path, 'model')
+                os.makedirs(models_path, exist_ok=True)
+                print(f"Saving model in {models_path}...")
+                # Save the best model for each fold
+                for i, model in enumerate(self.best_models):
+                    model_filename = os.path.join(models_path, f'{self.model_name}_fold_{i+1}_model_default.pkl')
+                    with open(model_filename, 'wb') as f:
+                        pickle.dump(model, f)
+                print(f"Best models for all folds saved successfully in {models_path}.")
+            else:
+                print(f"Results saved successfully in {save_path}. Models not saved.")
+            
---
+++ src/run_classic_ml_default_batchscale.py
@@ -0,0 +1,147 @@
+from models_classes.default_classic_ml_models_kfolds_batchscale import ClassicMLDefault
+import argparse
+import os
+import json
+
+def run_on_datasets(main_dir, model, kfold_method, granularity_level, random_state, n_jobs_model, model_kwargs, verbose, scaling, scale_batch_col, dumb_columns):
+    """
+    This function runs the selected model on all datasets in the main directory using the argparse arguments.
+    """
+    # Extracting the keywords from json files
+    if model_kwargs: #optional input
+        with open(model_kwargs, 'r') as f:
+            model_kwargs_dict = json.load(f)
+    else:
+        model_kwargs_dict = {}
+
+    if scaling == 'Yes':
+        scaling = True
+    else:
+        scaling = False
+    
+    if dumb_columns is not None:
+        dumb_columns_clean = dumb_columns.strip()
+        if ',' in dumb_columns_clean:
+            dumb_columns = [s.strip() for s in dumb_columns_clean.split(',')]
+        else:
+            dumb_columns = dumb_columns_clean
+
+    # Starting the loop through datasets
+    for dataset_dir in os.listdir(os.path.join(main_dir, 'datasets')):
+        if not os.path.isdir(os.path.join(main_dir, 'datasets', dataset_dir)):
+            continue
+        if dataset_dir.startswith('.'):
+            continue
+        print(f"Processing dataset {dataset_dir}")
+        datasets_path = os.path.join(main_dir, "datasets", dataset_dir)
+        kfold_dir = os.path.join(datasets_path, f'quantification/processed/kfolds_{kfold_method}_{granularity_level}')
+        save_dir = os.path.join(main_dir, 'results', dataset_dir, f'{model}_default_{kfold_method}', granularity_level)
+        os.makedirs(save_dir, exist_ok=True)
+        label_dir = os.path.join(datasets_path, f'quantification/processed/labels_{kfold_method}_{granularity_level}.csv')
+
+        data_object = ClassicMLDefault(random_state=random_state, model = model, n_jobs = n_jobs_model, **model_kwargs_dict)
+        data_object.train_tune_evaluate(kfold_dir, label_dir, verbose, scaling, scale_batch_col, dumb_columns)
+        data_object.save_results(save_dir, label_dir, kfold_dir)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Run classic ML models on the benchmark datasets'
+        )
+    parser.add_argument(
+        "--main_dir",
+        type=str,
+        help="Path to the main directory holding the folders 'datasets' and 'results'. A specific directory structure is expected. See README for more information.",
+        required=True
+        )
+    parser.add_argument(
+        "--model",
+        type=str,
+        choices=['logistic_regression', 'random_forest', 'xgboost'],
+        help="Select a model to run, either 'logistic_regression', 'random_forest' or 'xgboost'", required=True
+        )
+    parser.add_argument(
+        "--kfold_method",
+        type=str,
+        choices=['StratifiedGroupKFold', 'StratifiedKFold', 'GroupShuffleSplit'],
+        default='StratifiedGroupKFold',
+        help="Which fold creation method was used. Default is StratifiedGroupKFold."
+        )
+    parser.add_argument(
+        "--granularity_level",
+        type=str,
+        choices=['level1', 'level2', 'level3'],
+        default='level3',
+        help="On which celltype granularity level the method is applied to. Granularity level 3 represents the finest granularity and equals cell subtypes. Default is level3."
+        )
+    parser.add_argument(
+        "--random_state",
+        type=int,
+        default=42,
+        help="Random state for reproducibility"
+        )
+    parser.add_argument(
+        "--n_jobs_model",
+        type=int,
+        default=-1,
+        help="Number of jobs passed to the model object itself" 
+        )
+    parser.add_argument(
+        "--model_kwargs",
+        type=str,
+        help="""Path to the json file containing the model kwargs, otherwise standard values are used. please consider using class_weight parameter as 'balanced, also for xgboost. Example:
+        {
+        "min_child_weight": 2,
+        "learning_rate": 0.05,
+        "class_weight": "balanced",
+        }
+        In XGBoost, the class_weight parameter is not directly available. We implemented it with compute_sample_weight parameter from sklearn. In Logistic Regression and Random Forest, the class_weight parameter is directly available.
+        """
+        )
+    parser.add_argument(
+        "--verbose",
+        type=int,
+        default=2,
+        help="Verbosity level for xgboost training and evluation"
+        )
+    parser.add_argument(
+        '--scaling',
+        type=str,
+        choices=['Yes', 'No'],
+        default='Yes',
+        help="Whether to scale the data using scikit-learn StandardScaler or not"
+        )
+    parser.add_argument(
+        '--scale_batch_col',
+        type=str,
+        default=None,
+        help="Column to perform batch-wise scaling. If not provided, scaling will be performed on the entire dataset."
+        )
+    parser.add_argument(
+        "--dumb_columns",
+        type=str,
+        help="Columns to drop from the data. Comma-seperated strings. Example: column1,column2",
+        default=None
+        )
+
+    args = parser.parse_args()
+    
+    print(f"Selected model: {args.model}")
+    
+    run_on_datasets(
+        main_dir=args.main_dir,
+        model=args.model,
+        kfold_method=args.kfold_method,
+        granularity_level=args.granularity_level,
+        random_state=args.random_state,
+        n_jobs_model=args.n_jobs_model,
+        model_kwargs=args.model_kwargs,
+        verbose=args.verbose,
+        scaling=args.scaling,
+        scale_batch_col=args.scale_batch_col,
+        dumb_columns=args.dumb_columns
+    )
+
+if __name__ == '__main__':
+    main()
