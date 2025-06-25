c1dace957a59f816298529badf2cfda4ea2ef94c
MargotCh <55975768+MargotCh@users.noreply.github.com>
2025-06-25 14:52:42 +0200

    add logging

--- src/clustering_methods/cHL2_clustering.R
+++ src/clustering_methods/cHL2_clustering.R
@@ -6,72 +6,84 @@ library(ggplot2)
 library(Rphenograph)
 library(FlowSOM) 
 library(reticulate)
+library(logging)
 ####
-#### prep ####
-# Set the path to your Python interpreter (optional but recommended)
+#### Logging setup ####
+# Ensure log directory exists
+if (!dir.exists("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/logs")) dir.create("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/logs", recursive = TRUE)
+# Configure logging
+basicConfig()
+addHandler(
+  writeToFile,
+  file = paste("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/logs/", Sys.Date(), "_clustering_chl2_run.log", sep = ""),
+  level = "INFO",
+  formatter = function(record) {
+    sprintf("%s - %s - %s",
+            format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
+            record$levelname,
+            record$msg)
+  }
+)
+
+#### Python setup ####
 use_condaenv('celllens-env')
-# Source the Python script
-source_python("greedy_f1_utils.py")
-#writing csv even if folder doesnt exist
+source_python("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/code/greedy_f1_utils.py")
+
+#### Utility: Safe CSV Write ####
 safe_write_csv <- function(df, path) {
   dir <- dirname(path)
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
   write.csv(df, path)
 }
 
+#### Config ####
 clustering <- c("phenograph_20","phenograph_30", "phenograph_40",
                 "phenograph_80","flowsom","flowsom_meta_clusters")
 granularity <- c("level1" = "level_1_cell_type","level2" ="level_2_cell_type","level3" ="cell_type")
 
-# read data
+#### Read data ####
 chl2 <- read_csv("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/chl2/cHL_2_MIBI_quantification.csv")
-####
+
+#### Main ####
 for (iteration in 1:5) {
   set.seed(iteration*42)
-  #### run phenograph ####
-  # run phenograph on chl2 data and overlay celltypes with clusters
-  pheno_chl2 <- select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA) %>%
-    Rphenograph(k = 30)
-  chl2$phenograph_30 <- factor(membership(pheno_chl2[[2]])) #results in 129 clusters
+  #### Run PhenoGraph at different k values ####
+  for (k_val in c(20, 30, 40, 80)) {
+    start_time <- Sys.time()
+    
+    pheno_chl2 <- select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA) %>%
+      Rphenograph(k = k_val)
+    chl2[[paste0("phenograph_", k_val)]] <- factor(membership(pheno_chl2[[2]]))
+    
+    end_time <- Sys.time()
+    loginfo("Rphenograph k = %d clustering (Duration: %s)",
+            k_val, 
+            paste0(round(as.numeric(difftime(end_time, start_time, units = "mins")), 2), " minutes"))
+  }
   
-  pheno_chl2 <- select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA) %>%
-    Rphenograph(k = 40)
-  chl2$phenograph_40 <- factor(membership(pheno_chl2[[2]])) #results in 128 clusters
-  
-  pheno_chl2 <- select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA) %>%
-    Rphenograph(k = 20)
-  chl2$phenograph_20 <- factor(membership(pheno_chl2[[2]])) #results in 137 clusters
-  
-  pheno_chl2 <- select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA) %>%
-    Rphenograph(k = 80)
-  chl2$phenograph_80 <- factor(membership(pheno_chl2[[2]])) #results in xx clusters
-
-  #### run FlowSOM clustering ####
-  # chl2 data 
+  #### Run FlowSOM clustering ####
+  start_time <- Sys.time()
   flowsom_chl2 <- FlowSOM(as.matrix(select(chl2, CD45:`Na-K ATPase`, -`Histone H3`, -dsDNA)), silent= FALSE, nClus = 12)
+  
+  end_time <- Sys.time()
+  loginfo("FlowSOM clustering (Duration: %s)", 
+          paste0(round(as.numeric(difftime(end_time, start_time, units = "mins")), 2), " minutes"))
+  
   chl2$flowsom <- GetClusters(flowsom_chl2)
   chl2$flowsom_meta_clusters <- GetMetaclusters(flowsom_chl2)
   ####
   
   #### greedy assignment ####
-  # # Show results
-  # cat("F1 macro:", results$f1_macro, "\n")
-  # cat("f1_weighted:", results$f1_weighted, "\n")
-  # cat("ARI:", results$ari, "\n")
-  # cat("NMI:", results$nmi, "\n")
-  # cat("mcc:", results$mcc, "\n")
-  # cat("kappa:", results$kappa, "\n")
-  # cat("Accuracy:", results$accuracy, "\n")
   for (i in clustering) {
     for (k in granularity) {
-      cat("Iteration", iteration, ":", "assigning", i, "to", k, "\n")
+      loginfo("Assigning %s to %s", i, k)
       results <- greedy_f1_score(chl2, k, i, tie_strategy = 'random')
-      cat("F1 macro:", results$f1_macro, "\n")
-      cat("f1_weighted:", results$f1_weighted, "\n")
-      output <- select(chl2, CD45:cell_type)
+      loginfo("F1 macro: %f", results$f1_macro)
+      loginfo("F1 weighted: %f", results$f1_weighted)
+      output <- select(chl2, CD45:cell_type, i)
       output$predicted_phenotype <- as.vector(results$mapped_predictions)
       output = rename(output, true_phenotype = k)
-      safe_write_csv(output, paste("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/chl2/", i, 
+      safe_write_csv(output, paste("/Users/margotchazotte/Documents/uni/PhD/pheno_benchmark/chl2/replace_chl2_if_successfull/", i, 
                                    "/", names(granularity[granularity == k]),
                                    "/predictions_", iteration,".csv", 
                                    sep = ""))
