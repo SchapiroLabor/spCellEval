1ec1d7e2fb07212323b747be285f559ef264f77a
Lukas Hatscher <lhatscher@protonmail.com>
2025-07-17 13:29:08 +0200

    adjusted astir an other files

--- src/astir/run_astir.py
+++ src/astir/run_astir.py
@@ -13,6 +13,8 @@ def run_astir(
     n_init,
     n_init_epochs,
     delta_loss,
+    learning_rate,
+    batch_size,
     random_seed,
     n_runs,
     output_path
@@ -26,7 +28,7 @@ def run_astir(
         ast = from_csv_yaml(os.path.join(output_path, "quantification_astir.csv"), marker_yaml=decision_matrix_path, random_seed=seed)
 
         start_time = time.time()
-        ast.fit_type(max_epochs=max_epochs, n_init=n_init, n_init_epochs=n_init_epochs, delta_loss=delta_loss)
+        ast.fit_type(max_epochs=max_epochs, n_init=n_init, n_init_epochs=n_init_epochs, delta_loss=delta_loss, learning_rate=learning_rate, batch_size=batch_size)
         end_time = time.time()
         elapsed_time = end_time - start_time
 
@@ -50,6 +52,8 @@ def main():
     parser.add_argument('--n_init', type=int, default=5, help='Number of initializations for training (default: 5).')
     parser.add_argument('--n_init_epochs', type=int, default=5, help='Number of epochs for each initialization (default: 5).')
     parser.add_argument('--delta_loss', type=float, default=0.001, help='Delta loss for convergence (default: 0.001).')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for training (default: 0.001).')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size for training (default: 128).')
     parser.add_argument('--random_seed', type=int, default=42, help='Random seed for reproducibility (default: 42).')
     parser.add_argument('--n_runs', type=int, default=5, help='Number of runs for the ASTIR algorithm (default: 5).')
     parser.add_argument('--output_path', type=str, required=True, help='Path to save the ASTIR results.')
@@ -63,6 +67,8 @@ def main():
         n_init=args.n_init,
         n_init_epochs=args.n_init_epochs,
         delta_loss=args.delta_loss,
+        learning_rate=args.learning_rate,
+        batch_size=args.batch_size,
         random_seed=args.random_seed,
         n_runs=args.n_runs,
         output_path=args.output_path
