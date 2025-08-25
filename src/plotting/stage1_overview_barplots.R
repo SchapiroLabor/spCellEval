4a77c0dae8cd5bf8229c0e75be55ac019b7b9a0c
Margot Chazotte <55975768+MargotCh@users.noreply.github.com>
2025-08-25 12:15:08 +0200

    update pie charts

--- src/plotting/stage1_overview_barplots.R
+++ src/plotting/stage1_overview_barplots.R
@@ -1,18 +1,22 @@
 library(tidyverse)
 library(ggplot2)
+install.packages("Cairo")
+library(Cairo)
+
+library(webr)
 
 #dataprep
 datasets <- tibble(
   platform = c(rep("MIBI", 2), rep("CODEX", 1), rep("IMC", 1), rep("Lunaphore", 1), "CODEX", "MIBI", rep("IMC", 2)),
   cells = c(1669853, 230895, 145161,100000,100000, 1200000, 500000, 553121, 457117), 
   tissue = c(rep("Lymph node", 3), "Bone marrow","Heart", "Bone marrow", "Decidua", "Breast",  "Multiple"),
-  stage1 = c("remaining", "pilot", rep("remaining", 6), "pilot")
+  stage1 = c("full", "pilot", rep("full", 6), "pilot")
 )
 datasets$platform <- factor(datasets$platform, levels = c("MIBI", "IMC","CODEX",  "Lunaphore"))
-datasets$tissue <- factor(datasets$tissue, levels = c("Lymph node", "Bone marrow", "Breast", "Decidua", "Multiple","Heart"))
-datasets$stage1 <- factor(datasets$stage1, levels = c("remaining", "pilot"))
+datasets$tissue <- factor(datasets$tissue, levels = c("Lymph node", "Bone marrow","Multiple", "Breast",  "Decidua", "Heart"))
+datasets$stage1 <- factor(datasets$stage1, levels = c( "pilot", "full"))
 
-#barplots
+#### barplots ####
 ggplot(datasets, aes(platform, cells, fill = stage1)) +
   geom_col(position = "stack", width = 0.8, color = "#3E424B") +
   scale_fill_manual(values = c( "grey", "#7AA2C4")) + #46a2da #006884
@@ -39,7 +43,7 @@ ggplot(datasets, aes(tissue, cells, fill = stage1)) +
   guides(fill = "none") +
   labs(x = "Tissue", y = "cells", title = "Cells per Tissue")
 
-#radial plot
+#### radial plot ####
 ggplot(datasets, aes(tissue, cells, fill = stage1)) +
   geom_col(position = "stack", width = 0.8, color = "#3E424B") +
   scale_fill_manual(values = c( "grey", "#7AA2C4")) +
@@ -53,7 +57,7 @@ ggplot(datasets, aes(tissue, cells, fill = stage1)) +
   labs(x = "Tissue", y = "cells", title = "Cells per Tissue") +
   coord_radial(theta = "x")
 
-#pie charts
+#### pie charts ####
 ggplot(datasets, aes(x = "", y = cells, fill = tissue, color = stage1)) +
   geom_col(width = 1) +
   scale_fill_brewer(palette = "YlGnBu", name = "Tissue")+
@@ -85,22 +89,41 @@ ggplot(datasets, aes(x = "", y = cells, fill = platform, color = stage1)) +
         legend.title = element_text(size = 15),
         plot.title = element_text(colour = "black", size = 20, hjust = 0.5))
 
+#### PieDonuts ####
 
-library(webr)
+#platform 
+platform <- datasets %>% group_by(platform, stage1) %>% summarise(n = sum(cells))
+platform$stage1 <- as.character(platform$stage1)
 
-data <- datasets %>% group_by(platform, stage1) %>% summarise(n = sum(cells))
-data$stage1 <- as.character(data$stage1)
-data$stage1[is.na(data$stage1)] <- "full"
-
-PieDonut(data, aes(platform, stage1, count=n), title = "Pilot: Cells per Platform", 
+PieDonut(platform, aes(platform, stage1, count=n), title = "Pilot: Cells per Platform", 
          #ratioByGroup = FALSE, 
          showRatioDonut = FALSE, showRatioPie = FALSE, 
-         showPieName = FALSE, 
+         showPieName = TRUE, 
          explodeDonut = TRUE,
          donutLabelSize = 4,
+         pieLabelSize = 4,
          selected = c(2,4))
 
-PieDonut(data, aes(stage1, platform, count=n), title = "Pilot: Cells per Platform", 
-         ratioByGroup = FALSE, 
-         showRatioDonut = FALSE, showRatioPie = FALSE, showPieName = FALSE, labelposition = 1,
-         explode = 2)
+# PieDonut(platform, aes(stage1, platform, count=n), title = "Pilot: Cells per Platform", 
+#          ratioByGroup = FALSE, 
+#          showRatioDonut = FALSE, showRatioPie = FALSE, showPieName = FALSE, labelposition = 1,
+#          explode = 2)
+
+#tissue
+organ <- datasets %>% group_by(tissue, stage1) %>% summarise(n = sum(cells))
+organ$stage1 <- as.character(organ$stage1)
+
+PieDonut(organ, aes(tissue, stage1, count=n), title = "Pilot: Cells per Tissue", 
+         #ratioByGroup = FALSE, 
+         showRatioDonut = FALSE, showRatioPie = FALSE, 
+         showPieName = TRUE, 
+         explodeDonut = TRUE,
+         donutLabelSize = 4,
+         pieLabelSize = 4,
+         selected = c(2,6,9))
+
+
+# PieDonut(organ, aes(stage1, tissue, count=n), title = "Pilot: Cells per Tissue", 
+#          ratioByGroup = FALSE, 
+#          showRatioDonut = FALSE, showRatioPie = FALSE, showPieName = FALSE, labelposition = 1,
+#          explode = 2)
